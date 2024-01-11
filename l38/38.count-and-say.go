0dc9b40398a15cd28dd2d0edaedbdc4ea2b28be5
OrkWard <orkward_o@outlook.com>
2024-01-11 19:36:29 +0800

    feat(38): medium

--- l38/38.count-and-say.go
+++ l38/38.count-and-say.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"strconv"
+)
+
 /*
  * @lc app=leetcode id=38 lang=golang
  *
@@ -8,7 +12,31 @@ package main
 
 // @lc code=start
 func countAndSay(n int) string {
-	return ""
+	if n == 1 {
+		return "1"
+	}
+
+	former := countAndSay(n - 1)
+	counter := 1
+	currNum := former[0]
+	say := ""
+
+	for _, num := range former[1:] {
+		if currNum == byte(num) {
+			counter += 1
+		} else {
+			say += strconv.Itoa(counter)
+			say += string(currNum)
+
+			counter = 1
+			currNum = byte(num)
+		}
+	}
+
+	say += strconv.Itoa(counter)
+	say += string(currNum)
+
+	return say
 }
 
 // @lc code=end
--- l38/main.go
+++ l38/main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(countAndSay(3))
+	for i := 1; i < 10; i += 1 {
+		fmt.Println(countAndSay(i))
+	}
 }
